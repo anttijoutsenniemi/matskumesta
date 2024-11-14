f7f4054b0c4fd9ed22ab74e1de072589ec8e91f5
Antti Joutsenniemi <tinpurittna@gmail.com>
2024-11-15 00:40:07 +0200

    new template for empty page

--- front/matskumesta/src/pages/EmptyPage.tsx
+++ front/matskumesta/src/pages/EmptyPage.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
 import useStore from '../stores/useStore';
-import { Typography } from '@mui/material';
+import { Typography, IconButton } from '@mui/material';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { useNavigate } from 'react-router-dom';
 
 const EmptyPage: React.FC = () => {
   const { username } = useStore();
-  return <div><Typography variant='h4'>Matskumesta</Typography></div>;
+
+  const navigate = useNavigate();
+
+  const backToPreviousPage = () => {
+    //replace with whatever page u want
+    navigate('/home');
+  }
+
+  return <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%' }}>
+            {/* Header Container */}
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: 'flex-start',
+                alignItems: 'center',
+                width: '100%',
+                padding: '16px 0',
+              }}
+            >
+              {/* Back Arrow and Text */}
+              <IconButton onClick={backToPreviousPage}>
+                <ArrowBackIosIcon />
+              
+              <Typography variant="body1">Takaisin</Typography>
+              </IconButton>
+            </div>
+
+            <div>
+              {/* Here paste page content */}
+            </div>
+
+          </div>
 };
 
 export default EmptyPage;
